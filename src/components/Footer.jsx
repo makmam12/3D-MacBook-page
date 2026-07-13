d3d815495159fb7d9cb34e1e19dbf4f397eab9ec
Makram <makmam12@github.com>
2026-07-13 18:16:54 +0300

    finished the page, i will get back when i master gsap animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="info">
+            <p>
+                More ways to shop: Find an Apple Store or other retailer near you. Or call
+            </p>
+            <img src="logo.svg" alt="" />
+                </div>
+            <hr />
+            <div className="links">
+                <p>Copyright 2024 Inc. all rights reserved.</p>
+                <ul>
+                    {footerLinks.map(({label, link}) => (
+                        <li key={label}>
+                            <a href={link}>{label}</a>
+                        </li>
+                    ))} 
+                </ul>
+            </div>
+    </footer>
   )
 }
 
